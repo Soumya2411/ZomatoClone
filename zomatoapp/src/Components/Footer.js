9b24e7c84e0de685a3249ff761328697d4ef48fc
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-04-26 22:21:46 +0530

    update

---
+++ zomatoapp/src/Components/Footer.js
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer>
+
+
+    
+
+
+
+        <p>Created by: </p>
+        <p>Soumya Gupta</p>
+        <p>© 2021 - 2022</p>
+  
+    </footer>
+  );
+}
+
+export default Footer;
